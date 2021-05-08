f7b489c71040fe8d64be14b2d15f9d42e39e01ae
Daniel Upchurch <danielupchurch93@gmail.com>
2021-05-08 09:23:12 -0400

    testing began

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,23 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import BuyListItem from "./BuyListItem"
+
+describe("The BuyListItem Component", () => {
+    describe("renders", () => {
+        beforeEach(() => {
+            const testProduct1 = {
+                "itemNumber": "1",
+                "name": "test1",
+                "image": "https://cdnimg.webstaurantstore.com/images/products/small/204813/1411756.jpg",
+                "description": "this is the first test object",
+                "price": 13.83
+            }
+
+            render(<BuyListItem product={testProduct1}/>)
+        })
+
+        
+    })
+})
